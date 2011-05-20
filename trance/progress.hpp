857404cd0a337587ac2b488e96c1883a37cf5328
Kohei Takahashi <flast@flast.jp>
2011-05-21 02:09:22 +0900

    simply decrementable progress display

--- trance/progress.hpp
+++ trance/progress.hpp
@@ -30,6 +30,8 @@
 
 #include <boost/noncopyable.hpp>
 
+#include <trance/iostreams/screen.hpp>
+
 namespace trance
 {
 
@@ -40,23 +42,47 @@ protected:
     typedef unsigned int   count_type;
     typedef ::std::ostream stream_type;
 
-    progress_display( count_type _expected, stream_type &_ostr ) TRANCE_NOEXCEPT
+    struct _construct_without_restart_tag {};
+    BOOST_STATIC_CONSTEXPR _construct_without_restart_tag _construct_without_restart
+#if defined( TRANCE_HAS_CONSTEXPR )
+      = _construct_without_restart_tag{}
+#endif // TRANCE_HAS_CONSTEXPR
+      ;
+
+private:
+    stream_type &_M_ostr;
+    count_type _M_count, _M_expected;
+
+protected:
+    progress_display(
+      _construct_without_restart_tag,
+      count_type _expected,
+      stream_type &_ostr ) TRANCE_NOEXCEPT
       : _M_ostr( _ostr ),
         _M_count( 0 ), _M_expected( _expected ? _expected : 1u ) {}
+
+    progress_display( count_type _expected, stream_type &_ostr ) TRANCE_NOEXCEPT
+      : _M_ostr( _ostr ),
+        _M_count( 0 ), _M_expected( _expected ? _expected : 1u )
+    { restart( _expected ); }
 
     stream_type &
     _ostr( void ) TRANCE_NOEXCEPT
     { return _M_ostr; }
+
+    inline count_type
+    _increment_count( unsigned int _x = 1u ) TRANCE_NOEXCEPT
+    { return _M_count += _x; }
+
+    inline count_type
+    _decrement_count( unsigned int _x = 1u ) TRANCE_NOEXCEPT
+    { return _M_count -= _x; }
 
     virtual void
     _restart_impl( void ) {}
 
     virtual void
     _display_tic_impl( void ) {}
-
-private:
-    stream_type &_M_ostr;
-    count_type _M_count, _M_expected;
 
 public:
     void
@@ -70,12 +96,12 @@ public:
     progress_display &
     operator+=( count_type _x )
     {
-        _M_count += _x;
+        _increment_count( _x );
         _display_tic_impl();
         return *this;
     }
 
-    inline progress_display &
+    progress_display &
     operator++( void )
     { return operator+=( 1u ); }
 
@@ -93,12 +119,25 @@ class boost_progress_display
   : public progress_display
 {
     const ::std::string _M_s1, _M_s2, _M_s3;
-    count_type _M_next_tic, _M_tic;
+    count_type _M_next_count;
 
-    void
+protected:
+    count_type _M_tic;
+
+    boost_progress_display(
+      _construct_without_restart_tag _flag,
+      count_type _expected,
+      stream_type &_ostr,
+      const ::std::string &_s1,
+      const ::std::string &_s2,
+      const ::std::string &_s3 )
+      : progress_display( _flag, _expected, _ostr ),
+        _M_s1( _s1 ), _M_s2( _s2 ), _M_s3( _s3 ) {}
+
+    virtual void
     _restart_impl( void )
     {
-        _M_next_tic = _M_tic = 0;
+        _M_next_count = _M_tic = 0;
 
         _ostr() << ::std::endl
           << _M_s1 << "0%   10   20   30   40   50   60   70   80   90   100%\n"
@@ -107,19 +146,19 @@ class boost_progress_display
           << _M_s3;
     }
 
-    void
+    virtual void
     _display_tic_impl( void )
     {
-        if ( count() < _M_next_tic )
+        if ( count() < _M_next_count )
         { return; }
 
-        const count_type tics_needed = count() * 50.0 / expected_count();
+        const count_type tics_next = count() * 50.0 / expected_count();
 
         do
         { _ostr() << '*' << ::std::flush; }
-        while ( ++_M_tic < tics_needed );
+        while ( ++_M_tic < tics_next );
 
-        _M_next_tic = _M_tic / 50.0 * expected_count();
+        _M_next_count = _M_tic / 50.0 * expected_count();
         if ( count() == expected_count() )
         {
             if ( _M_tic < 51 )
@@ -156,6 +195,66 @@ public:
     inline count_type
     operator++( void )
     { return operator+=( 1u ); }
+};
+
+class decrementable_progress_display
+  : public boost_progress_display
+{
+    count_type _M_prev_count;
+
+    void
+    _restart_impl( void )
+    {
+        _M_prev_count = 0;
+        boost_progress_display::_restart_impl();
+    }
+
+    void
+    _display_tic_impl( void )
+    {
+        const count_type tic = _M_tic ? _M_tic - 1 : _M_tic;
+        _M_prev_count = tic / 50.0 * expected_count();
+        if ( count() <= _M_prev_count )
+        {
+            const count_type tics_prev = count() * 50.0 / expected_count();
+
+            do
+            {
+                _ostr() << iostreams::move_left( 1 )
+                        << ' '
+                        << iostreams::move_left( 1 )
+                        << ::std::flush;
+            }
+            while ( --_M_tic > tics_prev );
+        }
+        boost_progress_display::_display_tic_impl();
+    }
+
+public:
+    explicit
+    decrementable_progress_display(
+      count_type _expected,
+      stream_type &_ostr = ::std::cout,
+      const ::std::string &_s1 = "",
+      const ::std::string &_s2 = "",
+      const ::std::string &_s3 = "" )
+      : boost_progress_display(
+          _construct_without_restart,
+          _expected, _ostr,
+          _s1, _s2, _s3 )
+    { restart( _expected ); }
+
+    count_type
+    operator-=( count_type _x )
+    {
+        _decrement_count( _x );
+        _display_tic_impl();
+        return _x;
+    }
+
+    count_type
+    operator--( void )
+    { return operator-=( 1u ); }
 };
 
 } // namespace trance
